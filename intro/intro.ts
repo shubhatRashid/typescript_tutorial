25c7d202f54ca13187b380489a236029818022df
shubhat33 <rashidshubhat41@gmail.com>
2024-05-16 11:02:18 +0530

    type inferance

--- intro/intro.ts
+++ intro/intro.ts
@@ -8,16 +8,29 @@
     // }
 
 // DECLEARING TYPE IN TS :
-    // {
-    //     let myName:string
-    //     myName = 'shubhat rashid' 
-    //     console.log(myName) // this gives 'shubhat' as output
+    {
+        let myName:string
+        myName = 'shubhat rashid' 
+        console.log(myName) // this gives 'shubhat' as output
 
-    //     // myName = 12345   // this gives error as string cant be a number
-    //     // console.log(myName)
+        // myName = 12345   // this gives error as string cant be a number
+        // console.log(myName)
         
-    //     // another way is :
-    //     let firstName:string = 'shubhat'
-    //     console.log(firstName)
-    // }
+        // another way is :
+        let firstName:string = 'shubhat'
+        console.log(firstName)
+
+        // numbers :
+        let userId : number = 33802145
+        console.log(userId)
+        // ts doesnot have int and float , both are just number.
+
+        // boolean :
+        let isLoggedIn : boolean = false
+
+        // Type inferance :
+            // the above decleration can be simplified as 
+            let myAge = 20 // typescript is built to detect types of them using their values
+            
+    }
 
