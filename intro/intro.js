9c97ab4eb12814099a1bf19b68867118b71812a5
shubhat33 <rashidshubhat41@gmail.com>
2024-05-16 10:50:02 +0530

    introduction to typescript types and good errors

--- intro/intro.js
+++ intro/intro.js
@@ -1,5 +1,19 @@
-var user = {
-    name: 'shubhat rashid',
-    age: 20
-};
-console.log(user);
+// GOOD ERRORS :
+// {
+//     var user = {
+//         name:'shubhat rashid',
+//         age : 20
+//     }
+//     console.log(user.email) // gives undefined on js but error on ts
+// }
+// DECLEARING TYPE IN TS :
+{
+    var myName = void 0;
+    myName = 'shubhat rashid';
+    console.log(myName); // this gives 'shubhat' as output
+    // myName = 12345   // this gives error as string cant be a number
+    // console.log(myName)
+    // another way is :
+    var firstName = 'shubhat';
+    console.log(firstName);
+}
--- intro/intro.ts
+++ intro/intro.ts
@@ -1,6 +1,23 @@
-var user = {
-    name:'shubhat rashid',
-    age : 20
-}
+// GOOD ERRORS :
+    // {
+    //     var user = {
+    //         name:'shubhat rashid',
+    //         age : 20
+    //     }
+    //     console.log(user.email) // gives undefined on js but error on ts
+    // }
 
-console.log(user)
+// DECLEARING TYPE IN TS :
+    // {
+    //     let myName:string
+    //     myName = 'shubhat rashid' 
+    //     console.log(myName) // this gives 'shubhat' as output
+
+    //     // myName = 12345   // this gives error as string cant be a number
+    //     // console.log(myName)
+        
+    //     // another way is :
+    //     let firstName:string = 'shubhat'
+    //     console.log(firstName)
+    // }
+
