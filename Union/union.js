86244e8f7961274f72441695260c20f83c884ac4
shubhat33 <rashidshubhat41@gmail.com>
2024-05-20 11:48:30 +0530

    types union in arrays

--- Union/union.js
+++ Union/union.js
@@ -7,18 +7,24 @@
 //     console.log(apiResponse)
 // } 
 // UNION IN FUNCTIONS
+// {
+//     function hello(arg :string|number){
+//         // arg.toUpperCase() gives error as it is not clear at this point if arg is string or number
+//         // arg += 2 also not allowed
+//         // no method related to numbers or string can be directly applied so we have to do a strict check first
+//         if (typeof arg === 'string'){
+//             return arg.toLocaleUpperCase()
+//         } 
+//         if (typeof arg === 'number'){
+//             return arg += 5
+//         } 
+//     }
+//     console.log(hello(5))
+//     console.log(hello('shubhat'))
+// }
+// UNION IN ARRAYS
 {
-    function hello(arg) {
-        // arg.toUpperCase() gives error as it is not clear at this point if arg is string or number
-        // arg += 2 also not allowed
-        // no method related to numbers or string can be directly applied so we have to do a strict check first
-        if (typeof arg === 'string') {
-            return arg.toLocaleUpperCase();
-        }
-        if (typeof arg === 'number') {
-            return arg += 5;
-        }
-    }
-    console.log(hello(5));
-    console.log(hello('shubhat'));
+    // let alphabetsAndnums:string = ['a','b','c',6] // gives error
+    var alphabetsAndnums = ['a', 'b', 'c', 6];
+    console.log(alphabetsAndnums);
 }
--- Union/union.ts
+++ Union/union.ts
@@ -10,20 +10,30 @@
     // } 
 
 // UNION IN FUNCTIONS
+    // {
+    //     function hello(arg :string|number){
+    //         // arg.toUpperCase() gives error as it is not clear at this point if arg is string or number
+    //         // arg += 2 also not allowed
+    //         // no method related to numbers or string can be directly applied so we have to do a strict check first
+    //         if (typeof arg === 'string'){
+    //             return arg.toLocaleUpperCase()
+    //         } 
+
+    //         if (typeof arg === 'number'){
+    //             return arg += 5
+    //         } 
+    //     }
+
+    //     console.log(hello(5))
+    //     console.log(hello('shubhat'))
+    // }
+
+// UNION IN ARRAYS
     {
-        function hello(arg :string|number){
-            // arg.toUpperCase() gives error as it is not clear at this point if arg is string or number
-            // arg += 2 also not allowed
-            // no method related to numbers or string can be directly applied so we have to do a strict check first
-            if (typeof arg === 'string'){
-                return arg.toLocaleUpperCase()
-            } 
-
-            if (typeof arg === 'number'){
-                return arg += 5
-            } 
-        }
-
-        console.log(hello(5))
-        console.log(hello('shubhat'))
+        // let alphabetsAndnums:string = ['a','b','c',6]                // gives error
+        // let alphabetsAndnums: string[] | number[] = ['a','b','c',6]  // wrong syntax as it specifies either a full array of strings or a
+        //                                                                 full array of numbers and not union of them
+        
+        let alphabetsAndnums:(string | number )[] = ['a','b','c',6]     // right syntax for union in arrays
+        console.log(alphabetsAndnums)
     }
