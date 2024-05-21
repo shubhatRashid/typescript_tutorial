f45792c09e27f4f7e67d3e2a6af76b6569e20062
shubhat33 <rashidshubhat41@gmail.com>
2024-05-21 16:10:00 +0530

    interface in ts

---
+++ objects/interface.js
@@ -0,0 +1,29 @@
+// Interface :
+{
+    var user = {
+        name: 'shubhat',
+        email: 'a@b.com',
+        age: 25,
+        otp: function () { return 12345; },
+        coupon: function (couponName) {
+            if (couponName === void 0) { couponName = 'shubhat'; }
+            return 20;
+        }
+    };
+    var user2 = {
+        name: 'shubhat',
+        email: 'a@b.com',
+        age: 25,
+        otp: function () { return 12345; },
+        coupon: function (couponName) {
+            if (couponName === void 0) { couponName = 'shubhat'; }
+            return 20;
+        },
+        address: 'jandk'
+    };
+    console.log(user);
+    console.log(user2);
+}
+// Interface vs Type
+{
+}
---
+++ objects/interface.ts
@@ -0,0 +1,39 @@
+// Interface :
+    {
+        // just like type alias we also have interface however their are differences between them
+        interface User {
+            name : string,
+            email: string,
+            nickName ? : string,
+            age : number
+            otp : () => number // can also be written as otp():number which is just specifying a function with number type return
+            coupon? :(couponName:string) => number
+        }
+        const user:User ={
+            name:'shubhat',
+            email:'a@b.com',
+            age : 25,
+            otp:() => 12345,
+            coupon:(couponName='shubhat') => 20
+        }
+        // interface can take new properties without touching the original decleration i.e
+        interface User {
+            address? : string
+        }
+
+        const user2:User ={
+            name:'shubhat',
+            email:'a@b.com',
+            age : 25,
+            otp:() => 12345,
+            coupon:(couponName='shubhat') => 20,
+            address : 'jandk'
+        }
+        console.log(user)
+        console.log(user2)
+    }
+
+// Interface vs Type
+    {
+
+    }
