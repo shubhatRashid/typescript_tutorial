f038acef4421c6ac6f7ef0d36863a25c189a3270
shubhat33 <rashidshubhat41@gmail.com>
2024-05-25 16:46:53 +0530

    alternative syntax for classes

--- constructors/constructors.ts
+++ constructors/constructors.ts
@@ -1,13 +1,39 @@
-class User {
-    name : string
-    public email : string
-    private password : string | number = 'hello'
-    constructor(name:string,email:string){
-        this.name = name,
-        this.email = email
-    }
-}
-const user = new User('shubhat','a@b')
-user.name = 'rashid shubhat'
-// console.log(user.password) gives error as password is private and is accessible inside class only
-// by default name is public 
+// BASIC SYNTAX
+// {   
+//     class User {
+//         name : string
+//         email : string
+//         readonly password : string | number = 'hello'
+//         constructor(name:string,email:string){
+//             this.name = name,
+//             this.email = email
+//         }
+//         }
+//         const user = new User('shubhat','a@b')
+//         user.name = 'rashid shubhat'
+//         console.log(user.password)
+// }
+
+// PRIVATE AND PUBLIC
+// {
+//     class User {
+//         public name : string // by default name would be public so mentioning it is useless
+//         public email : string // same as above
+//         private password:string|number // private variable only accessible inside class decleration and not outside
+//         constructor(name:string,email:string){
+//             this.name = name,
+//             this.email = email
+//         }
+//     }
+// }
+
+// ALTERNATE SYNTAX
+// {
+//     class User {
+//         private password:string|number
+//         constructor(public name:string, public email:string){
+//             this.name = name,
+//             this.email = email
+//         }
+//     }
+// }
