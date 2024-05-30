3824ab0730850493d943ba942650ac8c9825953f
shubhat33 <rashidshubhat41@gmail.com>
2024-05-30 15:52:35 +0530

    generics in arrow functions and arrays as input and output

--- generics/generics.js
+++ generics/generics.js
@@ -1,25 +1,38 @@
-/* suppose we need to create a function which accepts type of data and return same type of data
-   we could do something  like :
-*/
-function identity(data) {
-    return data;
+// GENERICS
+// {
+//     /* suppose we need to create a function which accepts type of data and return same type of data 
+//     we could do something  like :
+//     */
+//     function identity(data:string):string{
+//         return data
+//     }
+//     /*
+//     This however only takes one type of data i.e string what if our data is dynamic
+//     We could do something like :
+//     */
+//     function identity2(data:string | boolean | number):string | boolean | number{
+//         return data
+//     }
+//     function identity3(data:any):any{
+//         return data
+//     }
+//     /* 
+//     This however doesnot give any specific information about output as it can be any of the three
+//     and if there are more types of data it would elongate the code.We cannot use 'any' due to similar reasone.
+//     This is where type generics is used :
+//     */
+//     function identity4<newType>(data:newType):newType{
+//         return data
+//     }
+//     console.log(identity4<string>('hello')) // works as string type for input and output
+// }
+// GENERICS IN ARRAYS AND ARROW FUNCTIONS
+{
+    // Syntax for arrow functions
+    var identity = function (val) { return val; };
+    // Syntax for arrays
+    function identity2(data) {
+        return data;
+    }
+    console.log(typeof identity2([1, 2, 3, 4]));
 }
-/*
-This however only takes one type of data i.e string what if our data is dynamic
-We could do something like :
- */
-function identity2(data) {
-    return data;
-}
-function identity3(data) {
-    return data;
-}
-/*
-This however doesnot give any specific information about output as it can be any of the three
-and if there are more types of data it would elongate the code.We cannot use 'any' due to similar reasone.
-This is where type generics is used
-*/
-function identity4(data) {
-    return data;
-}
-console.log(identity4('hello')); // works as string type for input and output
