0ecdfb7bee56ee507d135c735550d5a51ffd8de2
shubhat33 <rashidshubhat41@gmail.com>
2024-06-04 15:07:36 +0530

    instanceOf

--- TypeNarrowing/typeNarrowing.ts
+++ TypeNarrowing/typeNarrowing.ts
@@ -13,5 +13,17 @@
     3) If there are multiple types of interfaces being used in a function, Use in operator to check
         if a specific property exists in current input.
 
-    
  */
+
+// INSTANCE_OF AND TYPE PREDICATES :
+    {
+        // InstanceOf is like typeOf but it is used with objects which can be created using new keyword
+        // e.g Date, Array etc
+        function logValue(x: Date | string) {
+            if (x instanceof Date) {
+              console.log(x.toUTCString());
+            } else {
+              console.log(x.toUpperCase());
+            }
+          }
+    }
